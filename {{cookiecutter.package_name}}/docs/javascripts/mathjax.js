352b73a9436e24cb2f4ffa1e0a1d10f0d8261222
Carlos M Silva <kakarotocm@hotmail.com>
2026-06-15 14:15:22 -0300

    feat: add MathJax config with dollar sign shorthand for inline and display math

---
+++ {{cookiecutter.package_name}}/docs/javascripts/mathjax.js
@@ -0,0 +1,13 @@
+window.MathJax = {
+  tex: {
+    // Added "$" for inline and "$$" for display math
+    inlineMath: [["\\(", "\\)"], ["$", "$"]],
+    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
